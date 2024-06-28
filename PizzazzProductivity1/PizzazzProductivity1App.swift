c79c4d0056756f88374207a307e8acb487389d3f
ZeinaRebouh <missionstreetcat@gmail.com>
2024-06-28 09:53:41 -0700

    Initial Commit

---
+++ PizzazzProductivity1/PizzazzProductivity1App.swift
@@ -0,0 +1,17 @@
+//
+//  PizzazzProductivity1App.swift
+//  PizzazzProductivity1
+//
+//  Created by Scholar on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct PizzazzProductivity1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
